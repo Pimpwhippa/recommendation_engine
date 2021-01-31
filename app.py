2f22a3cf348e0c6fb1807d49184ca291e8afd6c7
pimpwhippa <utrechtmay@gmail.com>
2021-01-31 23:29:47 +0200

    add port variable for heroku to assign

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def recommend():
     #return render_template('recom_titles.html', data=recom_movie_titles.to_html())
 
 if __name__ == '__main__':
-        app.run(debug=False,host='0.0.0.0', port=5002)
+        port = int(os.environ.get("PORT", 5000))
+        app.run(debug=False,host='0.0.0.0', port=port)
